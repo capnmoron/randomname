ac33f73d5f554c1c66fd126b4ce918b936e55c54
Akshit Goyal <akshit2392@gmail.com>
2022-02-12 17:29:57 +0530

    front

---
+++ random/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+        <button type="button" class="btn my-3 mx-8 btn-primary">Dealer</button>
+        <button type="button" class="btn my-3 mx-8 btn-primary">Driver</button>
+    </>
+  )
+}
+
+export default Home
